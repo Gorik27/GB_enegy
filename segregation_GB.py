e823ac0c5930fbc2202539daf2f920fa7159efb1
Georgiy Marchiy <gmar@head1.hpc>
2023-03-07 14:48:27 +0300

    modifications for restart

--- segregation_GB.py
+++ segregation_GB.py
@@ -38,121 +38,139 @@ def main(args):
     if (os.path.abspath(os.getcwd()).split('/'))[-1]!='scripts':
         os.chdir('scripts')
     print(os.getcwd())
-    
-    task = (f'{lmp} -in in.segregation ' + mu_arg +
-            f'-var name {name} ' + 
-            f'-var structure_name {structure} ' +
-            f'-var conc_f {args.conc} -var kappa_f {args.kappa} ' + 
-            f'-pk omp {job} ' +
-            f'-sf omp')
 
-    print(task)
+    if args.restart:
+        routine = 'in.segregation_r'
+        struct_flag = ''
+    else:
+        routine = 'in.segregation'
+        struct_flag = f'-var structure_name {structure} '
 
-    err_flag = False
-    counter = 0
-    N_conv = 0
-    file_count = args.zero_count
-    N_conv_tot = 0
-    last_counter = 0
-    datfile = ''
-    exitflag = False
-    with Popen(task.split(), stdout=PIPE, bufsize=1, universal_newlines=True) as p:
-        time.sleep(0.1)
-        print('\n')
-        for line in p.stdout:
-            if 'ERROR' in line:
-                raise ValueError(f'ERROR in LAMMPS: {line}')
-            if 'thermo output file:' in line:
-                src_path = line.split()[-1]
-                src = src_path.split('/')[-1]
-            elif "dumpfile" in line:
-                dumpfile = (line.replace('dumpfile ', '')).replace('\n', '')
-            elif "datfile" in line:
-                datfile = (line.replace('datfile ', '')).replace('\n', '')
-            elif "vcsgc_loop" in line:
-                counter += 1
-                print('loop', counter)
-            elif "Per-node simulation cell is too small for fix sgcmc" in line:
-                raise ValueError(line)
+    restart_flag = True
 
-            if nonverbose:
-                if '!' in line:
-                    print(line.replace('!', ''), end='')
-            else:
-                print(line, end='')   
-            if (counter != last_counter) and (counter%N_loops == 0):
-                last_counter = counter
-                impath = f'../workspace/{name}/images'
-                Path(impath).mkdir(exist_ok=True)  
-                parser_ = argparse.ArgumentParser()
-                plot_args = parser_.parse_args('')
-                fname = f'../workspace/{name}/segregarion_plot.txt'
-                flag1=False
-                flag2=False
-                flag3=False
-                flag4=False
-                flag5=False
-                flag6=False
-                with open(fname, 'r') as f :
-                    for line in f:
-                        if 'slope width' in line:
-                            plot_args.w = int(line.split()[-1])
-                            flag1=True
-                        if 'step' in line:
-                            plot_args.st = int(line.split()[-1])
-                            flag2=True
-                        if 'rolling mean width' in line:
-                            plot_args.num = int(line.split()[-1])
-                            flag3=True
-                        if 'offset' in line:
-                            plot_args.s1 = int(line.split()[-1])
-                            flag4=True
-                        if 'converged slope' in line:
-                            slope_conv = float(line.split()[-1])
-                            flag5=True
-                        if 'number of points for convergence' in line:
-                            N_conv_criteria = int(line.split()[-1])
-                            flag6=True
-                flag = (flag1 and flag2 and flag3 and flag4 and flag5 and flag6)
-                if not flag:
-                    raise ValueError(f'incorrect segregarion_plot.txt')
-                
-                plot_args.name = args.name
-                plot_args.src = src
-                plot_args.hide = (not args.plot)
-                plot_args.slope_conv = slope_conv
-                plot_args.postfix = args.postfix
-                slope, E_mean = plot(plot_args)
-                slope = np.array(slope)
-                _N_conv_tot = np.sum(np.abs(slope)<=slope_conv)
-                if _N_conv_tot > N_conv_tot:
-                    N_conv += (_N_conv_tot-N_conv_tot)
-                N_conv_tot = _N_conv_tot
-                if len(slope)>0:
-                    if slope[-1]>slope_conv:
-                        N_conv = 0
+    while restart_flag:
+        task = (f'{lmp} -in  {routine} ' + mu_arg +
+                f'-var name {name} ' + 
+                struct_flag +
+                f'-var conc_f {args.conc} -var kappa_f {args.kappa} ' + 
+                f'-pk omp {job} ' +
+                f'-sf omp')
+
+        print(task)
+
+        counter = 0
+        N_conv = 0
+        file_count = args.zero_count
+        N_conv_tot = 0
+        last_counter = 0
+        datfile = ''
+        exitflag = False
+        
+        with Popen(task.split(), stdout=PIPE, bufsize=1, universal_newlines=True) as p:
+            time.sleep(0.1)
+            print('\n')
+            for line in p.stdout:
+                if 'ERROR' in line:
+                    raise ValueError(f'ERROR in LAMMPS: {line}')
+                if 'thermo output file:' in line:
+                    src_path = line.split()[-1]
+                    src = src_path.split('/')[-1]
+                elif "dumpfile" in line:
+                    dumpfile = (line.replace('dumpfile ', '')).replace('\n', '')
+                elif "datfile" in line:
+                    datfile = (line.replace('datfile ', '')).replace('\n', '')
+                elif "vcsgc_loop" in line:
+                    if restart_flag:
+                        restart_flag = False
+                    counter += 1
+                    print('loop', counter)
+                elif "Per-node simulation cell is too small for fix sgcmc" in line:
+                    raise ValueError(line)
+
+                if nonverbose:
+                    if '!' in line:
+                        print(line.replace('!', ''), end='')
+                else:
+                    print(line, end='')   
+                if (counter != last_counter) and (counter%N_loops == 0):
+                    last_counter = counter
+                    impath = f'../workspace/{name}/images'
+                    Path(impath).mkdir(exist_ok=True)  
+                    parser_ = argparse.ArgumentParser()
+                    plot_args = parser_.parse_args('')
+                    fname = f'../workspace/{name}/segregarion_plot.txt'
+                    flag1=False
+                    flag2=False
+                    flag3=False
+                    flag4=False
+                    flag5=False
+                    flag6=False
+                    with open(fname, 'r') as f :
+                        for line in f:
+                            if 'slope width' in line:
+                                plot_args.w = int(line.split()[-1])
+                                flag1=True
+                            if 'step' in line:
+                                plot_args.st = int(line.split()[-1])
+                                flag2=True
+                            if 'rolling mean width' in line:
+                                plot_args.num = int(line.split()[-1])
+                                flag3=True
+                            if 'offset' in line:
+                                plot_args.s1 = int(line.split()[-1])
+                                flag4=True
+                            if 'converged slope' in line:
+                                slope_conv = float(line.split()[-1])
+                                flag5=True
+                            if 'number of points for convergence' in line:
+                                N_conv_criteria = int(line.split()[-1])
+                                flag6=True
+                    flag = (flag1 and flag2 and flag3 and flag4 and flag5 and flag6)
+                    if not flag:
+                        raise ValueError(f'incorrect segregarion_plot.txt')
                     
-                print('convergence criteria achieved in', N_conv, 'points')
+                    plot_args.name = args.name
+                    plot_args.src = src
+                    plot_args.hide = (not args.plot)
+                    plot_args.slope_conv = slope_conv
+                    plot_args.postfix = args.postfix
+                    slope, E_mean = plot(plot_args)
+                    slope = np.array(slope)
+                    _N_conv_tot = np.sum(np.abs(slope)<=slope_conv)
+                    if _N_conv_tot > N_conv_tot:
+                        N_conv += (_N_conv_tot-N_conv_tot)
+                    N_conv_tot = _N_conv_tot
+                    if len(slope)>0:
+                        if slope[-1]>slope_conv:
+                            N_conv = 0
+                        
+                    print('convergence criteria achieved in', N_conv, 'points')
 
-                if N_conv >= N_conv_criteria:
-                    if datfile == '':
-                        print('Error: unrecognized datfile')
-                    else:
-                        print(f'saving state for sampling: {file_count+1}')
-                        file = datfile.replace("\n", "")
-                        outfile = file.replace('.dat', '') + f'_n{file_count}.dat'
-                        fpath = f'../workspace/{name}/dat/{file}'  
-                        dest = f'../workspace/{name}/samples'
-                        Path(dest).mkdir(exist_ok=True)  
-                        shutil.copyfile(fpath, f'{dest}/{outfile}')
-                        file_count+=1
-                        if file_count >= args.samples:
-                            exitflag = True
-                            p.kill()
-                            print('All done!')
-    if not exitflag:
-        print('\n!!!!!!!!!!!!!!!!!\n\nError occured in LAMMPS')
-        raise ValueError('Error in LAMMPS, check input script and log file')
+                    if N_conv >= N_conv_criteria:
+                        if datfile == '':
+                            print('Error: unrecognized datfile')
+                        else:
+                            print(f'saving state for sampling: {file_count+1}')
+                            file = datfile.replace("\n", "")
+                            outfile = file.replace('.dat', '') + f'_n{file_count}.dat'
+                            fpath = f'../workspace/{name}/dat/{file}'  
+                            dest = f'../workspace/{name}/samples'
+                            Path(dest).mkdir(exist_ok=True)  
+                            shutil.copyfile(fpath, f'{dest}/{outfile}')
+                            file_count+=1
+                            if file_count >= args.samples:
+                                exitflag = True
+                                p.kill()
+                                print('All done!')
+        if not exitflag:
+            print('\n!!!!!!!!!!!!!!!!!\n\nError occured in LAMMPS')
+            if restart_flag:
+                raise ValueError('Error in LAMMPS, check input script and log file')
+            else:
+                print('\n!!!!!!!!!!!!!!!!!\n\nRestarting simulation\n\n!!!!!!!!!!!!!!!!!\n\n')
+                restart_flag=True  
+                routine = 'in.segregation_r'
+                struct_flag = ''
 
 
 if __name__ == '__main__':
@@ -161,6 +179,7 @@ if __name__ == '__main__':
     parser.add_argument("-s", "--structure", required=False, default=False)
     parser.add_argument("-v", "--verbose", required=False, default=False, action='store_true',
                         help='show LAMMPS outpt')
+    parser.add_argument("-r", "--restart", required=False, default=False, action='store_true')
     parser.add_argument("-j", "--job", required=False, default=1)
     parser.add_argument("-m", "--mean-width", dest='mean_width', required=False, default=50)
     parser.add_argument("-c", "--conc", required=False, default=-1, type=float)
