6a647a6f719b5c42af1c724b0d20f730fffa3b36
MarchiyGV <Marchiy.Georgiy@yandex.ru>
2023-10-27 12:36:08 +0300

    dont remember what it is

--- submit.sh
+++ submit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 name=$1
-cat > task << EOF
+cat > task_$name << EOF
 #!/usr/bin/env bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=94
@@ -10,7 +10,9 @@ module purge
 module load intel
 module load openmpi3
 
-python -u berendsen_mpi.py -n 5g --heat --T0  300 -s berendsen_relax_T300_steps5000000.dat --np 27 -j 3 > $name.out
-python -u segregation_range_mpi.py -n 5g -s berendsen_relax_T600_steps5000000.dat --mu 1.23 -c1 0 -c2 5 -N 10 -k 100 --loops 30 --samples 5 --np 27 -j 3 > $name.out
+#python -u create.py -n $name -j 3 > ${name}_create.out
+#python -u berendsen_init_mpi.py -n ${name} -j 3 --np 27 > ${name}_init_berendsen.out
+python -u berendsen_mpi.py -n ${name} -j 3 --np 27 > ${name}_berendsen.out
+python -u segregation_range_mpi.py -n $name -s berendsen_relax_T300_steps5000000.dat --mu 1.23 -c1 0 -c2 5 -N 12 -k 100 --loops 30 --samples 1 --np 27 -j 3 > ${name}_seg.out
 EOF
-sbatch task
+sbatch task_$name
