f8f8688ebaa2b0a69874a2cdfd7a0788412a4e86
MarchiyGV <Marchiy.Georgiy@yandex.ru>
2024-06-24 16:35:03 +0300

    rewritted segregation_cooling.py

--- segregation_cooling.py
+++ segregation_cooling.py
@@ -9,138 +9,197 @@ from set_lammps import lmp
 from plot_segregation import main as plot
 import logging
 from datetime import datetime
+from collections.abc import Iterable
 now = datetime.now()
 current_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
+def log(*msg):
+    print(*msg)
+    if isinstance(msg, Iterable):
+        logging.info(' '.join(map(str, msg)))
+    else:
+        logging.info(str(msg))
+    
+def select_job_partition(args, structures):
+    N = len(structures)
+    if args.select:
+        if args.task != '1_1':
+            log("""
+!!!!!!!!!!!!!!!!!          WARNING          !!!!!!!!!!!!!!!!!!
 
-def main(args):
-    nonverbose = (not args.verbose)
-    job = args.job
-    name = args.name
-    structure = args.structure
-    N_loops = args.loops
-    kappa = args.kappa
-    if args.kappa == -1:
-        with open(f'workspace/{name}/input.txt', 'r') as f:
-            for line in f:
-                if 'variable kappa equal' in line:
-                    kappa = float(line.split(' ')[-1])
-    logname = f'segregation_range_cooled_k_{kappa}'
-    logging.basicConfig(filename=f'workspace/{name}/{logname}.log', 
-                        encoding='utf-8', 
-                        format='%(message)s',
-                        level=logging.INFO)
+    Argumet --task will be ignored since --select is used  
+                
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+""")
+        conc = list(map(float, args.select))
+        _structures = []
+        for s in structures:
+            if float(s.split('_')[1]) in conc:
+                _structures.append(s)
+        structures = _structures
+        log('Selected files:', str(structures))
+    else:
+        rank, ntasks = map(int, args.task.split('_'))
+        assert rank <= ntasks
+        assert rank > 0
+        assert ntasks > 0
 
-    logging.info(f"""
+        if ntasks>1:
+            mask = (np.arange(N)%ntasks==(rank-1)).astype(bool)
+            structures = list(np.array(structures)[mask])
+            log('Rank:', rank, '    Total tasks: ', ntasks)
+            log('Selected files:', str(structures))
+        conc = [float(s.split('_')[1]) for s in structures]
+    return conc, structures
+    
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--name", required=True, help='for example STGB_210')
+parser.add_argument("-s", "--structure", required=False, default=False)
+parser.add_argument("-v", "--verbose", required=False, default=False, action='store_true',
+                    help='show LAMMPS outpt')
+parser.add_argument("-r", "--restart", required=False, default=False, action='store_true')
+parser.add_argument("-j", "--job", required=False, default=1)
+parser.add_argument("--task", required=False, default="1_1", 
+                    help='"n_m": do n-th job from m jobs')
+parser.add_argument("--select", required=False, default=[], nargs='+',
+                    help='instead of using --task, this argument allow to explisitly select concentrations')
+parser.add_argument("--np", required=False, default=1)
+parser.add_argument("-m", "--mean-width", dest='mean_width', required=False, default=50)
+parser.add_argument("--mu", required=False, default=None, type=float)
+parser.add_argument("-k", "--kappa", required=False, default=-1, type=float)
+parser.add_argument("-p", "--plot", required=False, default=False, action='store_true',
+                    help='show the thermodynamic plot')
+parser.add_argument("--loops", required=False, default=100, type=int, dest='N_loops',
+                    help='draw the thermodynamic plot each <N> loops')
+parser.add_argument("--postfix", required=False, default='', help="add this postfix at the end of output file's names")
+args = parser.parse_args()
+
+name = args.name
+
+if args.select:
+    tasklabel = 'conc'+('_'.join(args.select))
+else:
+    tasklabel = args.task
+logname = 'segregation_range_cooled_'+tasklabel
+print(logname+'.log')
+logging.basicConfig(filename=f'workspace/{name}/logs/{logname}.log', 
+                    encoding='utf-8', 
+                    format='%(message)s',
+                    level=logging.INFO)
+
+logging.info(f"""
 ################
     START
 ################
 {current_time}
 ################
-    """)
-    
-    if args.mu:
-        mu_arg = f'-var mu0 {args.mu} '
-    else:
-        mu_arg = ''
+""")
 
-    print(f'workspace/{name}/dat/segregation_*_k_{float(args.file_kappa)}.dat')
-    structures = []
-    for filename in glob(f'workspace/{name}/dat/segregation_*_k_{float(args.file_kappa)}.dat'):
-        print(filename)   
+# selecting structure files, jobs spreading between tasks 
+structures = []
+for filename in glob(f'workspace/{name}/dat/segregation_[0-9]*.dat'):
         structures.append(filename.split('/')[-1])
 
+structures = sorted(structures,
+                    key=(lambda s: float(s.split('_')[1])))
 
+log('All files:', str(structures))
 
-    print(name, '\n')
-    print(structures)
-    logging.info(f'{name}\n')
-    logging.info(f'{structures}\n')
+concs, structures = select_job_partition(args, structures)
 
-    print(os.getcwd())
-    logging.info(os.getcwd())
-    if (os.path.abspath(os.getcwd()).split('/'))[-1]!='scripts':
-        os.chdir('scripts')
-    print(os.getcwd())
-    logging.info(os.getcwd())
+# change directory
+if (os.path.abspath(os.getcwd()).split('/'))[-1]!='scripts':
+    os.chdir('scripts')
 
-    step_ind = 0
+# GB group
+id_file = f'../workspace/{args.name}/dump/CNA/GBs.txt'
+gb_list = np.loadtxt(id_file).astype(int)[:,0]
+gb_list_arg = (''.join(f'{id} 1\n' for id in gb_list))
+gb_list_arg = f'{len(gb_list)}\n{gb_list_arg}'
+gb_list_file = f'../workspace/{args.name}/dump/CNA/GB_group_{tasklabel}.txt'
+with open(gb_list_file, 'w') as f:
+    f.write(gb_list_arg)
 
-    id_file = f'../workspace/{args.name}/dump/CNA/GBs.txt'
-    gb_list = np.loadtxt(id_file).astype(int)[:,0]
-    gb_list_arg = (''.join(f'{id} 1\n' for id in gb_list))
-    gb_list_arg = f'{len(gb_list)}\n{gb_list_arg}'
-    gb_list_file = f'../workspace/{args.name}/dump/CNA/GB_group.txt'
-    with open(gb_list_file, 'w') as f:
-        f.write(gb_list_arg)
+# output file; select files that has not been calculated
+output_file = f"../workspace/{args.name}/segregation_coolded_{tasklabel}{args.postfix}.txt"
+if not os.path.isfile(output_file):
+    with open(output_file, 'a') as f:
+        f.write('#segregation cooling output\n')
+        f.write('c,E,mu\n')
+else:
+    with open(output_file, 'r') as f:
+        lines = f.read().split('\n')
+        while '' in lines: lines.remove('')
+        for line in lines:
+            if not 'c,cavg,E,mu' in line and not '#' in line:
+                point = line.split(',')
+                conc = float(point[0])
+                cavg = float(point[1])
+                mu = float(point[3])
+                if conc in concs:
+                    i = concs.index(conc)
+                    concs.remove(conc)
+                    log(structures[i], 'is already calculated')
+                    structures.remove(structures[i])
+                    
+log('Files to be calculated:', structures)
 
-    output_file = f"../workspace/{args.name}/segregation_coolded_{args.postfix}.txt"
-    if not os.path.isfile(output_file):
-        with open(output_file, 'a') as f:
-            f.write('segregation range output\n')
-            f.write('c,E,mu\n')
+# restart
+restart = False 
+
+if restart:
+    routine = 'in.segregation_gb_cooling_r'
+else:
+    routine = 'in.segregation_gb_cooling'
+
+# routine
+
+if args.job == 1:
+    suffix = ''
+else:
+    suffix = f' -sf omp -pk omp {args.job} '
+
+for i, structure in enumerate(structures):
+    struct_flag = f'-var structure_name {structure} '
+
+    mu_arg = args.mu
+    if not mu_arg:
+        thermo = glob(f"../workspace/{args.name}/thermo_output/{structure.replace('.dat', '.txt')}")[0]
+        if os.path.isfile(thermo):
+            with open(thermo, 'r') as f:
+                lines = f.read().split('\n')
+                while '' in lines: lines.remove('')
+                if not '#' in lines[-1]:
+                    last_mu = float(lines[-1].split('; ')[-1])
+        mu_arg = f'-var mu0 {last_mu} '
+    if args.kappa == -1:
+        kappa = float(structure.split('_')[-1].replace('.dat', ''))
     else:
-        with open(output_file, 'r') as f:
-            lines = f.read().split('\n')
-            while '' in lines: lines.remove('')
-            print(lines)
-            if not 'c,E,mu' in lines[-1]:
-                last_point = lines[-1].split(',')
-                last_conc = float(last_point[0])
-                last_mu = float(last_point[2])
-                step_ind = len(lines)-1
-                args.restart = True
-    conc = float(structures[step_ind].split('_')[1]) 
-    structure = structures[step_ind]
+        kappa = args.kappa
 
-    if args.restart:
-        routine = 'in.segregation_gb_cooling_r'
-        struct_flag = ''
-        if mu_arg == '':
-            thermo = f"../workspace/{args.name}/thermo_output/segregation_{conc}_k_{args.file_kappa}.txt"
-            if os.path.isfile(thermo):
-                with open(thermo, 'r') as f:
-                    lines = f.read().split('\n')
-                    while '' in lines: lines.remove('')
-                    if not '#' in lines[-1]:
-                        last_mu = float(lines[-1].split('; ')[-1])
-            mu_arg = f'-var mu0 {last_mu} '
-    else:
-        routine = 'in.segregation_gb_cooling'
-        
+    conc = concs[i]
 
     restart_flag = True
-
     while restart_flag:
-        struct_flag = f'-var structure_name {structure} '
-        
-        if args.job == 1:
-            suffix = ''
-        else:
-            suffix = f' -sf omp -pk omp {args.job} '
-        
-        task = (f'mpirun -np {args.np} lmp_intel_cpu_openmpi -in  {routine} ' + mu_arg +
+        task = (f'mpirun -np {args.np} {lmp} -in {routine} ' + mu_arg +
                 f'-var name {name} ' + 
                 struct_flag + ' ' +
                 f'-var gb_list_file {gb_list_file} ' +
-                f'-var conc_f {conc} -var kappa_f {args.kappa} ' + 
+                f'-var conc_f {conc} -var kappa_f {kappa} ' + 
                 suffix)
-
-        msg = task
-        print(msg)
-        logging.info(msg)
+                
 
         counter = 0
         last_counter = 0
         exitflag = False
         
-        print("Starting LAMMPS procedure...\n")
-        logging.info("Starting LAMMPS procedure...\n")
+        log("Starting LAMMPS procedure...\n")
+        log(task)
 
         with Popen(task.split(), stdout=PIPE, bufsize=1, universal_newlines=True) as p:
             time.sleep(0.1)
-            print('\n')
-            logging.info('\n')
+            log('\n')
             for line in p.stdout:
                 if 'ERROR' in line:
                     raise ValueError(f'ERROR in LAMMPS: {line}')
@@ -153,28 +212,24 @@ def main(args):
                     datfile = (line.replace('datfile ', '')).replace('\n', '')
                 elif "mu = " in line:
                     mu = float((line.replace('\n', '')).split(' ')[-1])
+                elif "avg_conc = " in line:
+                    avg_conc = float((line.replace('\n', '')).split(' ')[-1])
                 elif "vcsgc_loop" in line:
                     if restart_flag:
                         restart_flag = False
                     counter += 1
-                    msg = f'loop {counter}'
-                    print(msg)
-                    logging.info(msg)
+                    log(f'loop {counter}')
                 elif "Per-node simulation cell is too small for fix sgcmc" in line:
                     raise ValueError(line)
                 elif "All done" in line:
                     exitflag = True
 
-                if nonverbose:
+                if not args.verbose:
                     if '!' in line:
-                        msg = line.replace('!', '').replace('\n', '')
-                        print(msg)
-                        logging.info(msg)
+                        log(line.replace('!', '').replace('\n', ''))
                 else:   
-                    msg = line.replace('\n', '')
-                    print(msg)
-                    logging.info(msg)
-                if (counter != last_counter) and (counter%N_loops == 0):
+                    log(line.replace('\n', ''))
+                if (counter != last_counter) and (counter%args.N_loops == 0):
                     last_counter = counter
                     impath = f'../workspace/{name}/images'
                     Path(impath).mkdir(exist_ok=True)  
@@ -216,48 +271,16 @@ def main(args):
                     slope, E_mean = plot(plot_args) # slope - eV/atom/MC step; E_mean - eV/atom
                     
         if not exitflag:
-            msg = '\n!!!!!!!!!!!!!!!!!\n\nError occured in LAMMPS'
-            print(msg)
-            logging.info(msg) 
+            log('\n!!!!!!!!!!!!!!!!!\n\nError occured in LAMMPS') 
             if restart_flag:
                 raise ValueError('Error in LAMMPS, check input script and log file')
             else:
-                msg = '\n!!!!!!!!!!!!!!!!!\n\nRestarting simulation\n\n!!!!!!!!!!!!!!!!!\n\n'
-                print(msg)
-                logging.info(msg) 
+                log('\n!!!!!!!!!!!!!!!!!\n\nRestarting simulation\n\n!!!!!!!!!!!!!!!!!\n\n') 
                 restart_flag=True  
                 routine = 'in.segregation_gb_cooling_r'
                 struct_flag = ''
                 mu_arg = f'-var mu0 {mu} '
         else:
-            msg = 'success'
-            print(msg)
-            logging.info(msg) 
-            
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--name", required=True, help='for example STGB_210')
-    parser.add_argument("-s", "--structure", required=False, default=False)
-    parser.add_argument("-v", "--verbose", required=False, default=False, action='store_true',
-                        help='show LAMMPS outpt')
-    parser.add_argument("-r", "--restart", required=False, default=False, action='store_true')
-    parser.add_argument("-j", "--job", required=False, default=1)
-    parser.add_argument("--np", required=False, default=1)
-    parser.add_argument("-m", "--mean-width", dest='mean_width', required=False, default=50)
-    parser.add_argument("--mu", required=False, default=None, type=float)
-    parser.add_argument("-k", "--kappa", required=False, default=-1, type=float)
-    parser.add_argument("-p", "--plot", required=False, default=False, action='store_true',
-                        help='show the thermodynamic plot')
-    parser.add_argument("--loops", required=False, default=100, type=int,
-                        help='draw the thermodynamic plot each <N> loops')
-    parser.add_argument("--samples", required=False, default=1, type=int, help='how many samples to save')
-    parser.add_argument("--zero-count", dest='zero_count', required=False, default=0, type=int, 
-                        help='start numeration of saving samples from this number')
-    parser.add_argument("--postfix", required=False, default='', help="add this postfix at the end of output file's names")
-    parser.add_argument("--fk", dest='file_kappa', required=True, type=float, help="kappa from last step")
-    args = parser.parse_args()
-    main(args)
-
-
+            log('success') 
+            with open(output_file, 'w') as f:
+                f.write(f'{conc},{avg_conc},{E_mean},{mu}\n')
